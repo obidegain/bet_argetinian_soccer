37dba594f8f62a627ea503bf69d6bf1120173512
obidegain <octa.bidegain@gmail.com>
2023-03-29 15:55:57 -0300

    little change in signals

--- BetsSport/main/signals.py
+++ BetsSport/main/signals.py
@@ -5,9 +5,5 @@ from .models import Match, Bet
 @receiver(post_save, sender=Match)
 def upload_score_bets(sender, instance, **kwargs):
     bets = Bet.objects.filter(match=instance)
-    bet = Bet.objects.get(id=1)
-    bet.result_home = 9
-    bet.save()
     for bet in bets:
-        bet.result_away = 6
         bet.save()
